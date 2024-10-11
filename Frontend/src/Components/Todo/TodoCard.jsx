e47dc58d56dacdd42401b970cc54aad52f78ca33
Aditi020 <aditi.kumar.2.2.24@gmail.com>
2024-10-12 05:08:45 +0530

    Footer fixed

--- Frontend/src/Components/Todo/TodoCard.jsx
+++ Frontend/src/Components/Todo/TodoCard.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Row, Col, Card, CardTitle, CardText, Button } from 'reactstrap';
+import { FiEdit } from "react-icons/fi";
+
 
 const TodoCard = ({ title, body }) => {
   return (
@@ -7,7 +9,7 @@ const TodoCard = ({ title, body }) => {
       <CardTitle tag="h5">{title}</CardTitle>
       <CardText>{body}</CardText>
       <Row>
-     <Col ><Button className='Fun-btn'> Edit Todo</Button></Col>
+        <Col ><Button className='Fun-btn'> Edit Todo <FiEdit style={{marginLeft:"5px"}}/></Button></Col>
         <Col > <Button className='Fun-btn'>Delete Todo</Button></Col> 
       </Row>
     </Card>
